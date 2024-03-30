3b8f487fee731847f0aad20961ea40b12f772f48
Duna Seo <tjensk26@gmail.com>
2024-03-30 16:39:02 +0900

    Create 플로이드.java

---
+++ 서두나/BAEKJOON/플로이드.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11404 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		long[][] arr = new long[n + 1][n + 1];
+
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (i == j) {
+					arr[i][j] = 0;
+				} else {
+					arr[i][j] = Integer.MAX_VALUE;
+				}
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+
+			arr[a][b] = Math.min(arr[a][b], c);
+		}
+
+		for (int k = 1; k < n + 1; k++) {
+			for (int i = 1; i < n + 1; i++) {
+				for (int j = 1; j < n + 1; j++) {
+					arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+				}
+			}
+		}
+
+		for (int i = 1; i < n + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (arr[i][j] == Integer.MAX_VALUE) {
+					System.out.print(0 + " ");
+				} else {
+					System.out.print(arr[i][j] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
