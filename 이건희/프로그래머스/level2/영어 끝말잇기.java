6ebc15352d06bf1ccc47e487be610a6581976ff4
GeonHui2 <022686@naver.com>
2024-06-04 00:12:54 +0900

    Create 영어 끝말잇기.java

---
+++ 이건희/프로그래머스/level2/영어 끝말잇기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] result = {};
+        ArrayList<String> word = new ArrayList<>();
+        int cnt = 0;
+        
+        for (int i = 0; i < words.length; i++) {
+            cnt++;
+            if (word.length() == 0) word.append(words[i]);
+            else if (word[word.length() - 1][]) {
+                if ()
+                for (int j = 0; i < word.length(); j++) {
+                    if ()
+                }
+            }
+        }
+
+        return result;
+    }
+}
