94820e1938dfe42a62073a92851eb88958a00085
GeonHui2 <022686@naver.com>
2024-11-16 20:04:06 +0900

    Create Pipelined.java

---
+++ 이건희/소프티어/Lv3/Pipelined.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        
+        System.out.println(arr[N - 1] + N - 1);
+    }
+}
