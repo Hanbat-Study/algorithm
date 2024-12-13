829cfe74c6741e462d67657b739d6b9ace8aa060
Duna Seo <tjensk26@gmail.com>
2024-12-13 23:58:32 +0900

    Create 5월 식품들의 총매출 조회하기.sql

---
+++ 서두나/programmers/level4/5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT p.product_id, p.product_name, sum(o.amount)*p.price as total_sales from food_product p join food_order o on p.product_id = o.product_id
+where date_format(o.produce_date,'%Y-%m') = '2022-05'
+group by p.product_name
+order by total_sales desc, p.product_id asc
