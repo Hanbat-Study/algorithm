a3de9393671fa1aca4958cb0f33964839d3813bc
Duna Seo <tjensk26@gmail.com>
2024-12-17 23:21:21 +0900

    Create 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql

---
+++ 서두나/programmers/level3/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,12 @@
+select concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as file_path
+from used_goods_file 
+where 
+(
+    select board_id
+    from used_goods_board b
+    order by views desc
+    limit 1
+) 
+= board_id
+order by file_id desc
+;
